15e1b61e1b42c6741c4f38a39a23adc983829d55
MrArsenic621 <asedighi81@gmail.com>
2024-08-29 21:13:27 +0330

    save zustand stores to local storage

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,10 +1,11 @@
-import {create} from "zustand";
+import { create } from "zustand";
 import { TokenService } from "../openapi";
 import type {
   TokenObtainPairInputSchema,
   TokenObtainPairOutputSchema,
   TokenRefreshOutputSchema,
 } from "../openapi";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 interface AuthState {
   token: TokenObtainPairOutputSchema | TokenRefreshOutputSchema | null;
@@ -16,58 +17,66 @@ interface AuthState {
   logout: () => void;
 }
 
-const useAuthStore = create<AuthState>((set, get) => ({
-  token: null,
-  isAuthenticated: false,
-  error: null,
+const useAuthStore = create(
+  persist<AuthState>(
+    (set, get) => ({
+      token: null,
+      isAuthenticated: false,
+      error: null,
 
-  // Method to obtain a token
-  obtainToken: async (credentials) => {
-    try {
-      const response = await TokenService.tokenObtainPair(credentials);
-      set({ token: response, isAuthenticated: true, error: null });
-    } catch (error: any) {
-      set({ error: error.message });
-    }
-  },
+      // Method to obtain a token
+      obtainToken: async (credentials) => {
+        try {
+          const response = await TokenService.tokenObtainPair(credentials);
+          set({ token: response, isAuthenticated: true, error: null });
+        } catch (error: any) {
+          set({ error: error.message });
+        }
+      },
 
-  // Method to refresh the token
-  refreshToken: async () => {
-    const refreshToken = get().token?.refresh; // Assuming your token has a refresh property
-    if (!refreshToken) {
-      set({ error: "No refresh token available" });
-      return;
-    }
+      // Method to refresh the token
+      refreshToken: async () => {
+        const refreshToken = get().token?.refresh; // Assuming your token has a refresh property
+        if (!refreshToken) {
+          set({ error: "No refresh token available" });
+          return;
+        }
 
-    try {
-      const response = await TokenService.tokenRefresh({
-        refresh: refreshToken,
-      });
-      set({ token: response, error: null });
-    } catch (error: any) {
-      set({ error: error.message });
-    }
-  },
+        try {
+          const response = await TokenService.tokenRefresh({
+            refresh: refreshToken,
+          });
+          set({ token: response, error: null });
+        } catch (error: any) {
+          set({ error: error.message });
+        }
+      },
 
-  // Method to verify the token
-  verifyToken: async () => {
-    const token = get().token?.access; // Assuming your token has an access property
-    if (!token) {
-      set({ error: "No access token available" });
-      return;
-    }
+      // Method to verify the token
+      verifyToken: async () => {
+        const token = get().token?.access; // Assuming your token has an access property
+        if (!token) {
+          set({ error: "No access token available" });
+          return;
+        }
 
-    try {
-      await TokenService.tokenVerify({ token });
-      set({ error: null }); // Token is valid
-    } catch (error: any) {
-      set({ error: error.message });
+        try {
+          await TokenService.tokenVerify({ token });
+          set({ error: null }); // Token is valid
+        } catch (error: any) {
+          set({ error: error.message });
+        }
+      },
+      // Method to log out
+      logout: () => {
+        set({ token: null, isAuthenticated: false, error: null });
+      },
+    }),
+    {
+      name: "auth-storage", // name of the item in the storage (must be unique)
+      storage: createJSONStorage(() => localStorage), // (optional) by default, 'localStorage' is used
     }
-  },
-  // Method to log out
-  logout: () => {
-    set({ token: null, isAuthenticated: false, error: null });
-  },
-}));
+  )
+);
 
 export default useAuthStore;
--- src/store/themeStore.ts
+++ src/store/themeStore.ts
@@ -1,15 +1,25 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 interface ThemeState {
   mode: string;
   toggleMode: () => void;
 }
 
-const useThemeStore = create<ThemeState>((set, get) => ({
-  mode: "dark",
-  toggleMode:()=>{
-    get().mode === "dark"?set(()=>({mode:"light"})):set(()=>({mode:"dark"}))
-  }
-}));
+const useThemeStore = create(
+  persist<ThemeState>(
+    (set, get) => ({
+      mode: "dark",
+      toggleMode: () => {
+        const currentMode = get().mode;
+        set({ mode: currentMode === "dark" ? "light" : "dark" });
+      },
+    }),
+    {
+      name: "theme-storage", // name of the item in the storage (must be unique)
+      storage: createJSONStorage(() => localStorage), // (optional) by default, 'localStorage' is used
+    }
+  )
+);
 
 export default useThemeStore;
