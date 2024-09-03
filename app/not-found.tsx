738fa0d63d1cd0140dc151db83225e0df96f48ee
MrArsenic621 <asedighi81@gmail.com>
2024-09-03 17:25:39 +0330

    create routes for the app

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <Box>
+      <Typography variant="h2">صفحه مورد نظر شما یافت نشد.</Typography>
+    </Box>
+  );
+};
+
+export default NotFoundPage;
