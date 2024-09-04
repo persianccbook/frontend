43978ba51d817248a423c1a42b05fff8b874c794
MrArsenic621 <asedighi81@gmail.com>
2024-09-04 17:36:02 +0330

    add copyright to the footer

---
+++ app/components/CopyRightFooter.tsx
@@ -0,0 +1,28 @@
+import { Box, Link, Paper } from "@mui/material";
+import React from "react";
+
+const CopyRightFooter = () => {
+  const currentYear = new Date().getFullYear();
+  const yourName = "علیرضا صدیقی";
+  const yourLink = "";
+
+  return (
+    <Paper
+      elevation={0}
+      sx={{ display:"flex",flexDirection:"column",alignItems:'center'}}
+    >
+      <Box>
+        ساخته شده توسط
+        <Link href={yourLink} underline="none" sx={{ pr: 2 }}>
+          {yourName}
+        </Link>.
+        تمامی حقوق این وبسایت متعلق به کتابخانه آزاد پارسی می‌باشد.
+      </Box>
+      <Box sx={{ direction: "ltr" }}>
+        &copy; {currentYear}. All rights reserved.
+      </Box>
+    </Paper>
+  );
+};
+
+export default CopyRightFooter;
--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,12 +1,15 @@
+
 import { Box, Paper, Typography } from "@mui/material";
 import React from "react";
+import CopyRightFooter from "./CopyRightFooter";
 
 const Footer = () => {
   return (
     <Box sx={{ width: "100vw"}}>
-      <Paper>
+      <Paper elevation={5}>
         <Typography variant="h1">Footer</Typography>
       </Paper>
+      <CopyRightFooter/>
     </Box>
   );
 };
