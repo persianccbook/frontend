8df90e6a3562734099ca278f9172da24d8be0e0c
MrArsenic621 <asedighi81@gmail.com>
2024-09-04 16:03:27 +0330

    refactor: create a layout for auth route

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -9,7 +9,6 @@ import {
   FormLabel,
   InputAdornment,
   Link,
-  Paper,
   TextField,
   Typography,
 } from "@mui/material";
@@ -51,109 +50,122 @@ const LoginPage = () => {
   };
 
   return (
-    <Box display="flex" justifyContent="center" alignContent="center" marginBottom={3}>
-      <Paper sx={{ m: 5, p: 10, minWidth: 300, borderRadius: 5 }}>
-        <Typography variant="h3" sx={{ mb: 15 }}>
-          ورود
+    <>
+      <Typography variant="h3" sx={{ mb: 15 }}>
+        ورود
+      </Typography>
+      {apiError && (
+        <Typography variant="caption" color="error">
+          ایمیل یا گذرواژه وارد شده اشتباه است.
         </Typography>
-        {apiError && <Typography variant="caption" color="error">ایمیل یا گذرواژه وارد شده اشتباه است.</Typography>}
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <FormControl sx={{ minWidth: 290 }}>
-            <FormLabel sx={{ mb: 3 }}>ایمیل:</FormLabel>
-            <TextField
-              {...register("email")}
-              type="text"
-              dir="ltr"
-              placeholder="your@email.com"
-              sx={{ mb: 5 }}
-            />
-            {errors.email && (
-              <Typography variant="caption" color="error">
-                {errors.email.message}
-              </Typography>
-            )}
-            <FormLabel
-              sx={{ display: "flex", justifyContent: "space-between",alignItems:"center", mb: 3 }}
-            >
-              <Typography>گذرواژه:</Typography>
-              <NextLink href="/profile/reset-password" passHref>
-                <Link component="button" variant="caption" underline="none">
-                  گذرواژه خود را فراموش کرده اید؟
-                </Link>
-              </NextLink>
-            </FormLabel>
-            <TextField
-              {...register("password")}
-              type={isPasswordVisible ? "text" : "password"}
-              dir="ltr"
-              placeholder="********"
-              sx={{ mb: 5 }}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end" sx={{ pr: "0px" }}>
-                    <Button
-                      variant="contained"
-                      color="secondary"
-                      onClick={() => setPasswordVisibility(!isPasswordVisible)}
-                      sx={{
-                        p: 0,
-                        minWidth: 2,
+      )}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl sx={{ minWidth: 290 }}>
+          <FormLabel sx={{ mb: 3 }}>ایمیل:</FormLabel>
+          <TextField
+            {...register("email")}
+            type="text"
+            dir="ltr"
+            placeholder="your@email.com"
+            sx={{ mb: 5 }}
+          />
+          {errors.email && (
+            <Typography variant="caption" color="error">
+              {errors.email.message}
+            </Typography>
+          )}
+          <FormLabel
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              mb: 3,
+            }}
+          >
+            <Typography>گذرواژه:</Typography>
+            <NextLink href="/profile/reset-password" passHref>
+              <Link component="button" variant="caption" underline="none">
+                گذرواژه خود را فراموش کرده اید؟
+              </Link>
+            </NextLink>
+          </FormLabel>
+          <TextField
+            {...register("password")}
+            type={isPasswordVisible ? "text" : "password"}
+            dir="ltr"
+            placeholder="********"
+            sx={{ mb: 5 }}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end" sx={{ pr: "0px" }}>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    onClick={() => setPasswordVisibility(!isPasswordVisible)}
+                    sx={{
+                      p: 0,
+                      minWidth: 2,
+                      backgroundColor: "transparent",
+                      "&:hover": {
                         backgroundColor: "transparent",
-                        "&:hover": {
-                          backgroundColor: "transparent",
-                        },
-                      }}
-                      disableRipple
-                      disableElevation
-                    >
-                      {isPasswordVisible ? <VisibilityOff /> : <Visibility />}
-                    </Button>
-                  </InputAdornment>
-                ),
-              }}
-            />
-            {errors.password && (
-              <Typography variant="caption" color="error">
-                {errors.password.message}
-              </Typography>
-            )}
-            <Button
-              type="submit"
-              disabled={!isValid}
-              variant="contained"
-              sx={{ height: "3rem", fontWeight: "bold", my: 5 }}
-            >
-              ورود
-            </Button>
-            <Box sx={{ display: "flex", justifyContent: "space-evenly",alignItems:"center" }}>
-              <Typography variant="caption">حساب کاربری ندارید؟</Typography>
-              <NextLink href="/" passHref>
-                <Link component="button" variant="caption" underline="none">
-                  ثبت نام کنید
-                </Link>
-              </NextLink>
-            </Box>
-            <Divider>یا</Divider>
-            <Button
-              variant="contained"
-              color="secondary"
-              sx={{ height: "3rem", fontWeight: "bold", my: 5 }}
-            >
-              ورود با گوگل
-              <Google />
-            </Button>
-            <Button
-              variant="contained"
-              color="info"
-              sx={{ height: "3rem", fontWeight: "bold", my: 5 }}
-            >
-              ورود با گیتهاب
-              <GitHub />
-            </Button>
-          </FormControl>
-        </form>
-      </Paper>
-    </Box>
+                      },
+                    }}
+                    disableRipple
+                    disableElevation
+                  >
+                    {isPasswordVisible ? <VisibilityOff /> : <Visibility />}
+                  </Button>
+                </InputAdornment>
+              ),
+            }}
+          />
+          {errors.password && (
+            <Typography variant="caption" color="error">
+              {errors.password.message}
+            </Typography>
+          )}
+          <Button
+            type="submit"
+            disabled={!isValid}
+            variant="contained"
+            sx={{ height: "3rem", fontWeight: "bold", my: 5 }}
+          >
+            ورود
+          </Button>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-evenly",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="caption">حساب کاربری ندارید؟</Typography>
+            <NextLink href="/" passHref>
+              <Link component="button" variant="caption" underline="none">
+                ثبت نام کنید
+              </Link>
+            </NextLink>
+          </Box>
+          <Divider>یا</Divider>
+          <Button
+            variant="contained"
+            color="secondary"
+            sx={{ height: "3rem", fontWeight: "bold", my: 5 }}
+          >
+            ورود با گوگل
+            <Google />
+          </Button>
+          <Button
+            variant="contained"
+            color="info"
+            sx={{ height: "3rem", fontWeight: "bold", my: 5 }}
+          >
+            ورود با گیتهاب
+            <GitHub />
+          </Button>
+        </FormControl>
+      </form>
+    </>
   );
 };
 
