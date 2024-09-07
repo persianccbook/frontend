a4f1791e0e6419ed88deb8693d94e436b3d01655
MrArsenic621 <asedighi81@gmail.com>
2024-09-07 20:30:32 +0330

    implement verify email

---
+++ app/auth/verify-email/[user_id]/[token]/page.tsx
@@ -0,0 +1,34 @@
+import { ApiResponseSchema, AuthService, OpenAPI } from "../../../../../openapi";
+
+interface Props {
+  params: { user_id: string; token: string };
+}
+
+const VerifyEmailPage = async ({ params: { user_id, token } }: Props) => {
+  let res = {} as ApiResponseSchema;
+
+  OpenAPI.BASE='http://localhost'
+
+  const verifyEmail = async () => {
+    try {
+      res = await AuthService.apiAuthApiVerifyEmail({
+        user_id: user_id,
+        token: token,
+      });
+    } catch (error: any) {
+      console.log(error);
+    }
+    console.log(res);
+  };
+
+  await verifyEmail();
+
+  return (
+    <div>
+      {res?.status}
+      {res?.data && res?.data.message}
+    </div>
+  );
+};
+
+export default VerifyEmailPage;
