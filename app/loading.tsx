076ad1080e7432922f415bf578ff866d18735dc4
MrArsenic621 <asedighi81@gmail.com>
2024-09-15 21:50:28 +0330

    add spinner for loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,12 @@
+import { CircularProgress, Paper } from "@mui/material";
 import React from "react";
 
 const Loading = () => {
-  return <span className="loading loading-spinner loading-md"></span>;
+  return (
+    <Paper sx={{display:'flex',justifyContent:'center',alignItems:'center', position: "fixed", top: 0, left: 0, right: 0, bottom: 0,width:'100vw',height:'100vh',zIndex:20 }} >
+      <CircularProgress sx={{scale:3}}/>
+    </Paper>
+  );
 };
 
 export default Loading;
