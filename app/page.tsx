7c8e972ddf1ec21688e6c7a2f7aa24a467769c53
MrArsenic621 <asedighi81@gmail.com>
2024-09-15 17:23:42 +0330

    add a container for hoomepage contents

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
+import { Box } from "@mui/material";
 import IntroductionSegment from "./(home)/IntroductionSegment";
 
 const HomePage = () => {
   return (
-    <>
+    <Box sx={{display:'flex',justifySelf:'center',maxWidth:2000,m:'0 auto'}}>
       <IntroductionSegment />
-    </>
+    </Box>
   );
 };
 
