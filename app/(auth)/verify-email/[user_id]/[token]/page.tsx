75301f20bc927e0ce7822e3e6091a6c0ad110a13
MrArsenic621 <asedighi81@gmail.com>
2024-11-07 16:21:58 +0330

    fix showing errors and status of verification

--- app/(auth)/verify-email/[user_id]/[token]/page.tsx
+++ app/(auth)/verify-email/[user_id]/[token]/page.tsx
@@ -1,14 +1,16 @@
-'use client'
+"use client";
 import { useEffect, useState } from "react";
 import { ApiResponseSchema, AuthService } from "../../../../../openapi";
+import { AxiosError } from "axios";
+import { Box, Typography } from "@mui/material";
 
 interface Props {
   params: { user_id: string; token: string };
 }
 
-const VerifyEmailPage =  ({ params: { user_id, token } }: Props) => {
+const VerifyEmailPage = ({ params: { user_id, token } }: Props) => {
   const [res, setRes] = useState({} as ApiResponseSchema);
-
+  const [error, setError] = useState({} as AxiosError);
 
   useEffect(() => {
     const verifyEmail = async () => {
@@ -18,18 +20,30 @@ const VerifyEmailPage =  ({ params: { user_id, token } }: Props) => {
           token: token,
         });
         setRes(response);
-      } catch (error) {
-        console.error(error);
+      } catch (error: any) {
+        setError(error);
       }
+      console.log(error);
     };
     verifyEmail();
   }, [user_id, token]);
 
   return (
-    <div>
-      {res?.status}
-      {res?.data && res?.data.message}
-    </div>
+    <Box>
+      <Typography variant="h4" sx={{ mb: 10 }}>
+        تایید ایمیل
+      </Typography>
+      {error.message && (
+        <Typography variant="body1" color={"error"}>
+          خطا: {error.message}
+        </Typography>
+      )}
+      {res?.status === "success" && (
+        <Typography variant="body1" color={"primary"}>
+          ایمیل شما با موفقیت تایید و حساب کاربری شما فعال شد.
+        </Typography>
+      )}
+    </Box>
   );
 };
 
