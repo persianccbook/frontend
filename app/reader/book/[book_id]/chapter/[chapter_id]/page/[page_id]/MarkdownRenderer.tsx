4c7808e14701f11e9d79e387b00f95389f37688b
alireza <asedighi81@gmail.com>
2024-11-28 19:20:44 +0330

    fix rendering images in markdown

--- app/reader/book/[book_id]/chapter/[chapter_id]/page/[page_id]/MarkdownRenderer.tsx
+++ app/reader/book/[book_id]/chapter/[chapter_id]/page/[page_id]/MarkdownRenderer.tsx
@@ -8,11 +8,23 @@ interface Props {
   markdown: string;
 }
 
+// TODO: change to next image
 const MarkdownRenderer = ({ markdown }: Props) => {
+  const urlTransform = (url: string) => {
+    // Allow Base64 and any valid URLs
+    if (url.startsWith("data:image/")) return url; // Base64 image
+    return url; // Pass through other URLs
+  };
   return (
     <Markdown
       remarkPlugins={[remarkGfm]}
+      urlTransform={urlTransform}
       components={{
+        img: function img(props) {
+          return (
+            <img {...props} style={{ maxWidth: "100%", height: "auto" }} />
+          );
+        },
         code: function code(props) {
           const { children, className, node, ...rest } = props;
           const match = /language-(\w+)/.exec(className || "");
